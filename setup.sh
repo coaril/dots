241f7cd0dd359f410d640abbe66ad1c1d07fd96d
Coaril <294928377+coaril@users.noreply.github.com>
2026-09-06 14:19:00 -0400

    feat(scripts): add step logs to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
+GREEN="\e[1;32m"
+RESET="\e[0m"
+
+echo -e "[i]${GREEN} Updating system packages:${RESET}\n"
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
 # System packages
 PACKAGES=(
   # Dev tools
@@ -25,6 +34,7 @@ PACKAGES=(
   python3
 )
 
+echo -e "\n[i]${GREEN} Installing user packages (apt):${RESET}\n"
 sudo apt install -y "${PACKAGES[@]}"
 
 # Git
@@ -34,17 +44,21 @@ git config --global user.name "Coaril"
 git config --global user.email "294928377+coaril@users.noreply.github.com"
 
 # Brew packages
+echo -e "\n[i]${GREEN} Installing user packages (brew):${RESET}\n"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install nvm bun neovim uv
 
 # Node
+echo -e "\n[i]${GREEN} Installing node:${RESET}\n"
 export NVM_DIR="$HOME/.nvm"
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"
 nvm install --lts
 
 # Bun packages
+echo -e "\n[i]${GREEN} Installing user packages (bun):${RESET}\n"
 bun install -g vite npm-check-updates wrangler
 
+echo -e "\n[i]${GREEN} Setting up firewall:${RESET}\n"
 # Firewall
 sudo ufw --force enable
 sudo systemctl enable --now ufw
@@ -52,10 +66,14 @@ sudo ufw allow ssh
 sudo ufw reload
 
 # Default shell
+echo -e "\n[i]${GREEN} Setting default shell:${RESET}\n"
 sudo usermod --shell "$(which zsh)" "$USER"
 
 # Shell plugins
+echo -e "\n[i]${GREEN} Installing shell plugins:${RESET}\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/zsh-syntax-highlighting"
+
+echo -e "\n[i]${GREEN} Finalizing:${RESET}\n"
 
 # Copy dots
 cp -rt $HOME .tmux.conf .zsh* .config
@@ -71,3 +89,5 @@ ln -s $HOME/Code/Scripts/update.sh $HOME/.local/bin/update
 # Cleanup
 cd $HOME
 rm .bash*
+
+echo -e "\n[i]${GREEN} Done.${RESET}"
