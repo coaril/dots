c30f39c734639a5c887933ec691d32750760d6ca
Coaril <294928377+coaril@users.noreply.github.com>
2026-09-06 15:05:05 -0400

    refactor(scripts): reorganize packages and refine cleanup in setup.sh
    - Categorize apt packages into Core, System, Shell, Dev, and Misc
    - Remove redundant brew package installation block
    - Add force flag and .profile to home directory cleanup

--- setup.sh
+++ setup.sh
@@ -13,25 +13,29 @@ sudo apt autoremove -y
 
 # System packages
 PACKAGES=(
-  # Dev tools
+  # Core
   build-essential
-  util-linux
-  git
-  vim
-  # Sys Tools
-  ffmpeg
   curl
+  git
+  util-linux
   rsync
-  ufw
-  zsh
-  # Extra
-  eza
-  fastfetch
-  tmux
+  ffmpeg
+
+  # System
   btop
+  ufw
+
+  # Shell
+  eza
   zoxide
-  # Languages
-  python3
+  zsh
+
+  # Dev
+  vim
+  tmux
+
+  # Misc
+  fastfetch
 )
 
 echo -e "\n[i]${GREEN} Installing user packages (apt):${RESET}\n"
@@ -43,11 +47,6 @@ git config --global core.editor "nvim"
 git config --global user.name "Coaril"
 git config --global user.email "294928377+coaril@users.noreply.github.com"
 
-# Brew packages
-echo -e "\n[i]${GREEN} Installing user packages (brew):${RESET}\n"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install nvm bun neovim uv
-
 # Node
 echo -e "\n[i]${GREEN} Installing node:${RESET}\n"
 export NVM_DIR="$HOME/.nvm"
@@ -58,8 +57,8 @@ nvm install --lts
 echo -e "\n[i]${GREEN} Installing user packages (bun):${RESET}\n"
 bun install -g vite npm-check-updates wrangler
 
-echo -e "\n[i]${GREEN} Setting up firewall:${RESET}\n"
 # Firewall
+echo -e "\n[i]${GREEN} Setting up firewall:${RESET}\n"
 sudo ufw --force enable
 sudo systemctl enable --now ufw
 sudo ufw allow ssh
@@ -88,6 +87,6 @@ ln -s $HOME/Code/Scripts/update.sh $HOME/.local/bin/update
 
 # Cleanup
 cd $HOME
-rm .bash*
+rm -f .bash* .profile
 
 echo -e "\n[i]${GREEN} Done.${RESET}"
